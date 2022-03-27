9155dae9c1c8b5dcf829813de977c51bed735357
eulalaoliveira <larissa.brazil@live.com>
2022-03-27 12:28:20 -0300

    first commit

---
+++ src/context/AppProvider.jsx
@@ -0,0 +1,18 @@
+/**
+ * Context created to manage app's logic
+ * @author Larissa Oliveira Santos
+ */
+
+// -> colocar aqui as funções do app
+// decidir: createContext ou Redux ???
+// check imports e props !!!
+
+import React from 'react'
+
+export default function AppProvider() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/views/Container.jsx
@@ -0,0 +1,15 @@
+/**
+ * tic tac toe page container
+ * @author Larissa Oliveira Santos
+ */
+
+import React from 'react'
+
+export default function Container() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
