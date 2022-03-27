372132c79748b9c43fce67425cc4b173f056a8dc
eulalaoliveira <larissa.brazil@live.com>
2022-03-27 13:32:48 -0300

    style changes & components add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from "../components/ResetButton";
+
+export default function Footer() {
+    return (
+        <div></div>
+        /*<footer>
+            <h2 className="winner-message">
+                <span>won!</span>
+            </h2>
+
+            <Button />
+        </footer>*/
+    )
+}
--- src/views/Container.jsx
+++ src/views/Container.jsx
@@ -3,13 +3,15 @@
  * @author Larissa Oliveira Santos
  */
 
-import React from 'react'
-import Title from '../components/Title';
+import React from "react"
+import Title from "../components/Title";
+import Footer from "../components/Footer";
 
 export default function Container() {
   return (
     <main>
       <Title />
+      <Footer />
     </main>
   )
 }
